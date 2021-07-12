187eb537e6eaeeff067e40bb608800ec15c5d71a
Smael CHECK <smael.m.check@gmail.com>
2021-07-12 21:24:19 +0200

    Limite l'accessbilité de la classe que dans son 'package'

--- src/main/java/org/enchere/dal/DAO.java
+++ src/main/java/org/enchere/dal/DAO.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 
-public abstract class DAO <T extends Serializable> {
+ abstract class DAO <T extends Serializable> {
 
     /**
      * Obtenir un élément T par son id
